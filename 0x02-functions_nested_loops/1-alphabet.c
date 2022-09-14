700b33c06e9591dd70493ada9c9fafba5e3310b1
Tinsae <tinsaemesene@gmail.com>
2022-09-14 00:04:11 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,11 @@
 *main - print alphabets followed by new line
 *Return: Always 0 (success)
 */
-int print_alphabet(void)
+void print_alphabet(void)
 {
 char v;
 for (v = 'a'; v <= 'z'; v++)
 {
 _putchar(v);
 }
-return (0);
 }
