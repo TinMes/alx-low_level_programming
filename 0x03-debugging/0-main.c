67d606bb6cb41c3c5bed51d4db28d9073914d025
Tinsae <tinsaemesene@gmail.com>
2022-09-15 13:07:38 -0700

    edited task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,10 +3,10 @@
  * main - tests funtion that prints if integer is zero
  * Return: 0
  */
-int main(void)  
-{ 
-int i;  
-i = 0;  
-positive_or_negative(i);  
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
 return (0);
 }
