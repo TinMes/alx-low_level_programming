03d84306d90be1ed80760d9ddb25c371b08983de
Tinsae <tinsaemesene@gmail.com>
2022-09-22 14:42:01 -0700

    task 7 trial 3

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet- function that replaces some chatacters in a string
+ * @c: the pointer variable that holds the string
+ * Return: c
+ */
+char *leet(char *c)
+{
+int i, b, replacer[] = {'4', '3', '0', '7', '1'};
+int locate[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+for (i = 0; c[i] != '\0'; i++)
+{
+for (b = 0; b <= 9; b++)
+{
+if (c[i] == locate[b])
+{
+c[i] = replacer[b / 2];
+b = 9;
+}
+}
+}
+return (c);
+}
