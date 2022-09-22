d78aae3fb27a5c93ed2033747d6949adbfa34dd9
Tinsae <tinsaemesene@gmail.com>
2022-09-22 13:15:44 -0700

    task 1 t1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: The string to be copied from
+ * @src: The string to be appended to dest
+ * @n: number of bytes to used
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int a, c;
+a = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+for (c = 0; c < n; c++)
+{
+dest[a + c] = src[c];
+if (src[c] == '\0')
+{
+c = n;
+}
+return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 void print_number(int n);
 char *_strcat(char *dest, char *src);
+char *strncat(char *dest, char *src, int n);
