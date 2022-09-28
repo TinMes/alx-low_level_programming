0992525a9026f8308865dc1cc8614ed11957bf88
Tinsae <tinsaemesene@gmail.com>
2022-09-28 15:32:20 -0700

    Task 3 t 2

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: value to raise
- * @y: power
- * Return: result of the power
+ * factorial - returns the factorial of a number
+ * @n: number to return the factorial from
+ * Return: factorial of n
  */
-int _pow_recursion(int x, int y)
+int factorial(int n)
 {
-if (y < 0)
+if (n < 0)
 return (-1);
-if (y == 0)
+if (n == 0)
 return (1);
-return (x * _pow_recursion(x, y - 1));
+return (n * factorial(n - 1));
 }
